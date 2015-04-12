09343cf006b41c5e39050e0974dc142a5513658f
David Young-Chan Kay <davidykay@gmail.com>
2015-04-12 15:05:37 -0400

    using device service

--- app/conditions/conditions.js
+++ app/conditions/conditions.js
@@ -9,16 +9,11 @@ angular.module('myApp.conditions', ['ngRoute'])
   });
 }])
 
-.controller('ConditionsCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+.controller('ConditionsCtrl', ['$scope', '$routeParams', "deviceService", function($scope, $routeParams, deviceService) {
 
     $scope.astronautId = $routeParams.astronautId;
     $scope.bodypartId  = $routeParams.bodypartId;
 
-    $scope.conditions = [
-        {id:0, name: "Broken Arm",    img: "img/devices/arm_cast.jpg", model: "models/devices/arm_cast.obj"},
-        {id:1, name: "Broken Wrist",  img: "img/devices/arm_cast.jpg", model: "models/devices/arm_cast.obj"},
-        {id:2, name: "Broken Finger", img: "img/devices/arm_cast.jpg", model: "models/devices/arm_cast.obj"},
-        {id:3, name: "Tennis Elbow",  img: "img/devices/arm_cast.jpg", model: "models/devices/arm_cast.obj"},
-    ];
+    $scope.conditions = deviceService.devices;
 
 }]);
