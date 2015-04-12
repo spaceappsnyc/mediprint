1d9f3f95966bfc01a7728ab9e28003f079772425
David Young-Chan Kay <davidykay@gmail.com>
2015-04-12 16:25:30 -0400

    added ankle arm wrist render

--- app/components/astronauts/astronautService.js
+++ app/components/astronauts/astronautService.js
@@ -15,9 +15,9 @@ module.service('astronautService', function() {
 
 module.service('deviceService', function() {
     this.devices = [
-        {id:0, name: "Arm Cast",    img: "img/devices/arm_cast.jpg", model: "models/devices/arm_cast.obj"},
-        {id:1, name: "Wrist Brace",  img: "img/devices/brace_curved.jpg", model: "models/devices/brace_curved.obj"},
-        {id:2, name: "Finger Splint", img: "img/devices/finger_splint.jpg", model: "models/devices/finger_splint.obj"},
+        {id:0, name: "Arm Cast",      img: "img/devices/arm_cast_render.png",      model: "models/devices/arm_cast.obj"},
+        {id:1, name: "Wrist Brace",   img: "img/devices/wrist_brace_render.png",  model: "models/devices/brace_curved.obj"},
+        {id:2, name: "Finger Splint", img: "img/devices/finger_splint_render.png", model: "models/devices/finger_splint.obj"},
         // {id:3, name: "Elbow Brace",  img: "img/devices/arm_cast.jpg", model: "models/devices/arm_cast.obj"},
     ];
 });
