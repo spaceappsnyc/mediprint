fe606ec67ee8565046f4d062f021606dd694b0f0
David Young-Chan Kay <davidykay@gmail.com>
2015-04-12 00:13:07 -0400

    importing threejs in angular example.

--- app/device/device.js
+++ app/device/device.js
@@ -11,6 +11,17 @@ angular.module('myApp.device', ['ngRoute'])
 
 .controller('DeviceCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
 
+    $scope.assimpModelUrl = "models/jeep1.ms3d.json";
+
+    $scope.changeModel = function() {
+        if ($scope.assimpModelUrl == "models/interior.3ds.json") {
+            $scope.assimpModelUrl = "models/jeep1.ms3d.json";
+        }
+        else {
+            $scope.assimpModelUrl = "models/interior.3ds.json";
+        }
+    };
+
     $scope.astronautId = $routeParams.astronautId;
     $scope.bodyPartId  = $routeParams.bodyPartId;
     $scope.conditionId = $routeParams.conditionId;
