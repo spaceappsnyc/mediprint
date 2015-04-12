b051c7765c0cf60ed34df8d5dd089eb83c52a393
David Young-Chan Kay <davidykay@gmail.com>
2015-04-12 15:28:02 -0400

    got 3 standard models working.

--- app/components/astronauts/astronautService.js
+++ app/components/astronauts/astronautService.js
@@ -28,6 +28,15 @@ module.service('astronautService', function() {
     ];
 });
 
+module.service('deviceService', function() {
+    this.devices = [
+        {id:0, name: "Arm Cast",    img: "img/devices/arm_cast.jpg", model: "models/devices/arm_cast.obj"},
+        {id:1, name: "Wrist Brace",  img: "img/devices/brace_curved.jpg", model: "models/devices/brace_curved.obj"},
+        {id:2, name: "Finger Splint", img: "img/devices/finger_splint.jpg", model: "models/devices/finger_splint.obj"},
+        // {id:3, name: "Elbow Brace",  img: "img/devices/arm_cast.jpg", model: "models/devices/arm_cast.obj"},
+    ];
+});
+
 
 module.factory('notify', ['$window', function(win) {
    var msgs = [];
