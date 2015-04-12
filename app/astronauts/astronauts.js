a27aae75bb88600b26355c5cc9a90028a8b1adcb
David Young-Chan Kay <davidykay@gmail.com>
2015-04-12 08:54:05 -0400

    rough cut of device in left bar.

--- app/astronauts/astronauts.js
+++ app/astronauts/astronauts.js
@@ -17,7 +17,7 @@ angular.module('myApp.astronauts', ['ngRoute'])
         {id:0, name: "Cady Coleman",     img: "img/astronauts/cady_coleman.jpg"},
         {id:1, name: "Neil Armstrong",   img: "img/astronauts/neil_armstrong.jpg"},
         {id:2, name: "Douglas Wheelock", img: "img/astronauts/doug_wheelock.jpg"},
-        {id:3, name: "John Oquist",      img: "img/astronauts/john_oquist.jpg"},
+        //{id:3, name: "John Oquist",      img: "img/astronauts/john_oquist.jpg"},
         {id:4, name: "David Kay",        img: "img/astronauts/david_kay.png"},
     ];
 
