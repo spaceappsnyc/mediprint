34bc597b9dbfa79880d6c0ef1ad5e962b8f6c28e
David Young-Chan Kay <davidykay@gmail.com>
2015-04-12 01:09:24 -0400

    corrected helloTwo to use the right model.

--- app/mediprint/js/hellotwo.js
+++ app/mediprint/js/hellotwo.js
@@ -91,7 +91,7 @@ function init() {
     var objLoader = new THREE.OBJLoader( manager );
 
     //objLoader.load( 'models/male02.obj', function ( object ) {
-    objLoader.load( 'models/Male_full_body.obj', function ( object ) {
+    objLoader.load( 'models/astronauts/Male_full_body.obj', function ( object ) {
     //objLoader.load( 'models/man.obj', function ( object ) {
         object.traverse( function ( child ) {
             if ( child instanceof THREE.Mesh ) {
