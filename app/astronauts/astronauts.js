4f664d695426bcdb674b2bccc3b14c3d69bb2600
David Young-Chan Kay <davidykay@gmail.com>
2015-04-12 09:12:31 -0400

    updated wheelock photo and astronaut ids.

--- app/astronauts/astronauts.js
+++ app/astronauts/astronauts.js
@@ -12,13 +12,13 @@ angular.module('myApp.astronauts', ['ngRoute'])
 .controller('AstronautsCtrl', ['$scope', function($scope) {
 
     $scope.astronauts = [
-        {id:0, name: "Buzz Aldrin",      img: "img/astronauts/buzz_aldrin.jpg"},
-        {id:0, name: "John Glenn",       img: "img/astronauts/john_glenn.jpg"},
-        {id:0, name: "Cady Coleman",     img: "img/astronauts/cady_coleman.jpg"},
-        {id:1, name: "Neil Armstrong",   img: "img/astronauts/neil_armstrong.jpg"},
-        {id:2, name: "Douglas Wheelock", img: "img/astronauts/doug_wheelock.jpg"},
+        {id:0, name: "Neil Armstrong",   img: "img/astronauts/neil_armstrong.jpg"},
+        {id:1, name: "Buzz Aldrin",      img: "img/astronauts/buzz_aldrin.jpg"},
+        {id:2, name: "John Glenn",       img: "img/astronauts/john_glenn.jpg"},
+        {id:3, name: "Cady Coleman",     img: "img/astronauts/cady_coleman.jpg"},
+        {id:4, name: "Douglas Wheelock", img: "img/astronauts/doug_wheelock.jpg"},
         //{id:3, name: "John Oquist",      img: "img/astronauts/john_oquist.jpg"},
-        {id:4, name: "David Kay",        img: "img/astronauts/david_kay.png"},
+        {id:5, name: "David Kay",        img: "img/astronauts/david_kay.png"},
     ];
 
 }]);
